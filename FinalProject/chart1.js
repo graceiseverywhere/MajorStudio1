7552fd5fdfe0822a5769a404c2bdefe779f7b5aa
graceiseverywhere <martg029@newschool.edu>
2017-05-15 23:48:45 -0400

    final

---
+++ FinalProject/chart1.js
@@ -0,0 +1,12 @@
+var dataArray = [5,11,18];
+
+var svg = d3.select(".chart1").append("svg").attr("height","200").attr("width","1160");
+
+svg.selectAll("rect")
+      .data(dataArray)
+      .enter().append("rect")
+                .attr("height",function(d,i){ return d*15; })
+                .attr("width","50")
+                .attr("fill","pink")
+                .attr("x",function(d,i){ return 60*i; })
+                .attr("y",function(d,i){ return 300-(d*15); });
